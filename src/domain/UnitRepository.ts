daaa8e765c98c21cddfdb1126a566281cfa119e7
Felix Saaro <felix@saaro.ch>
2022-12-05 12:31:36 +0100

    Add method for UnitRepository

--- src/domain/UnitRepository.ts
+++ src/domain/UnitRepository.ts
@@ -1,3 +1,26 @@
+import IUnit from './IUnit';
+
 class UnitRepository {
-  
+  getUnitFromname(name: string): IUnit {
+    throw 'Not implemented yet';
+  }
+  listUnitDimension(): string[] {
+    return [];
+  }
+
+  listQuanitytClasses(): string[] {
+    return [];
+  }
+
+  listQuantityClassForUnit(unit: string): string[] {
+    return [];
+  }
+
+  listAliasForUnit(unit: string): string[] {
+    return [];
+  }
+
+  createSubQuantityClass(parent: string, className: string): void {
+    return;
+  }
 }
