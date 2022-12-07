6ff3a464afedef6e76d37f32904f700e3814836e
Felix Saaro <felix@saaro.ch>
2022-12-07 11:00:10 +0100

    Add ecosystem file

---
+++ api/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API',
+      cwd: `${__dirname}`,
+      script: 'C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js',
+      args: 'run start',
+    }
+  ]
+}
